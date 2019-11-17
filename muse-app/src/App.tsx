cf4676d2876d99e6a1969dfd98a2573ca3a37523
Miles Fertel <milesfertel@college.harvard.edu>
2019-11-17 00:59:49 -0500

    Structure

--- muse-app/src/App.tsx
+++ muse-app/src/App.tsx
@@ -2,31 +2,68 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  const generateMidi = (() => {
-    return;
-  });
+enum Mode {
+  WAIT,
+  LISTEN,
+  PLAY,
+  FAIL,
+  SUCCEED
+};
 
-  const playMidi = (() => {
-    return;
-  });
+interface Props {
+  handleClick: any;
+};
 
-  const handlePlayClick  = (() => {
-    console.log("helloooooo");
-  });
+interface State {
+  gameMode: Mode;
+  midi: number[];
+  noteCounter: number;
+};
 
+const PlayButton: React.FC<Props> = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Muse!
-        </p>
-
-      <button onClick={handlePlayClick}>Play a tune</button>
-      </header>
-    </div>
+    <button onClick={props.handleClick}>
+      Play a tune
+    </button>
   );
+};
+
+class App extends React.Component {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      gameMode: Mode.WAIT,
+      midi: [],
+      noteCounter: 0,
+    };
+  }
+
+  generateMidi = () => {
+    return [];
+  };
+
+  playMidi = () => {
+    return;
+  };
+
+  handlePlayClick: any = () => {
+    console.log("hellooooo");
+    return;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Welcome to Muse!
+          </p>
+          <PlayButton handleClick={this.handlePlayClick}></PlayButton>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
