1da91f7e5cfaefb0dadf44f8b89c326e6d55d24e
Miles Fertel <milesfertel@college.harvard.edu>
2019-11-17 01:31:13 -0500

    Comment

--- muse-app/src/App.tsx
+++ muse-app/src/App.tsx
@@ -1,49 +1,9 @@
 import React from 'react';
-import WebMidi from 'webmidi';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  const generateMidi = (() => {
-    
-    let context = new AudioContext();
-    let osc = context.createOscillator();
+import * as webmidi from "webmidi";
 
-    //create an array of eight random frequencies
-    let noteArray = new Array(8);
-    for (var i = 0; i < 8; i++) {
-      //generate MIDI number between 36 & 84 (C2 thru C6)
-      noteArray[i] = Math.floor(Math.random() * 84) + 36;
-      //convert note identifier to frequency
-      noteArray[i] = Math.pow(2, (noteArray[i]-69)/12)*440;
-    }
-    
-    return noteArray;
-  });
-
-  const playMidi = (() => {
-    let noteArray = generateMidi();
-    WebMidi.enable(function (err) {
-
-      if (err) {
-        console.log("WebMidi could not be enabled.", err);
-      } else {
-        console.log(WebMidi.outputs[0]);
-        let output = WebMidi.outputs[0];
-        for (var i = 0; i < noteArray.length; i++){
-          output.playNote(noteArray[i]);
-        }
-      }
-      
-    });
-
-    return;
-  });
-
-  const handlePlayClick  = (() => {
-    console.log("helloooooo");
-    playMidi();
-  });
 enum Mode {
   WAIT,
   LISTEN,
@@ -79,6 +39,32 @@ class App extends React.Component<{}, State> {
       noteCounter: 0,
     };
   }
+
+  generateMidi = () => {
+    //create an array of eight random frequencies
+    let noteArray = new Array(8);
+    for (var i = 0; i < 8; i++) {
+      //generate MIDI number between 36 & 84 (C2 thru C6)
+      noteArray[i] = Math.floor(Math.random() * 84) + 36;
+    }
+
+    return noteArray;
+  };
+
+  playMidi = (midi: number[]) => {
+    WebMidi.enable(function (err) {
+      if (err) {
+        console.log("WebMidi could not be enabled.", err);
+      } else {
+        console.log(WebMidi.outputs[0]);
+        let output = WebMidi.outputs[0];
+        for (var i = 0; i < noteArray.length; i++){
+          // Change this to have a delay and duration
+          output.playNote(noteArray[i]);
+        }
+      }
+    });
+  };
 
   handlePlayClick: any = () => {
     let midiNotes = this.generateMidi();
