1680131acc6b286933964b36056200f194783c6f
Miles Fertel <milesfertel@college.harvard.edu>
2019-11-17 00:04:45 -0500

    button

--- muse-app/src/App.tsx
+++ muse-app/src/App.tsx
@@ -3,21 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+  const playTune = (() => {
+    console.log("helloooooo");
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Welcome to Muse!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <button onClick={playTune}>Play a tune</button>
       </header>
     </div>
   );
